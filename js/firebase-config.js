507db1a5065675e44bbb2438b2be8d76e7a010a7
jofforka <54028942+jofforka@users.noreply.github.com>
2026-06-21 17:02:46 +0100

    Create firebase-config.js

---
+++ js/firebase-config.js
@@ -0,0 +1,21 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDH5p5LyIjimkHpIvJy2Jh5rkPSYzylzS8",
+  authDomain: "biserry-groceries-os.firebaseapp.com",
+  projectId: "biserry-groceries-os",
+  storageBucket: "biserry-groceries-os.firebasestorage.app",
+  messagingSenderId: "758590070159",
+  appId: "1:758590070159:web:6a0effae0aa74662d5fffa"
+};
+
+export const ADMIN_EMAILS = [
+  "admin@biserry.com"
+];
+
+export const BUSINESS = {
+  name: "Biserry Groceries",
+  phone: "+234 810 058 4211",
+  whatsapp: "2348100584211",
+  address: "Amac Market, Abuja",
+  instagram: "@biserry_groceries",
+  currency: "NGN"
+};
