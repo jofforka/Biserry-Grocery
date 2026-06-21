663ac1e49f2259a23503628a9d78ec16956d0fae
jofforka <54028942+jofforka@users.noreply.github.com>
2026-06-21 02:49:19 +0100

    Create firebase.js

---
+++ js/firebase.js
@@ -0,0 +1,16 @@
+// Firebase setup placeholder.
+// Paste your Firebase configuration here when ready.
+
+// import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+// import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+// import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+// import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
+
+// const firebaseConfig = {
+//   apiKey: "YOUR_API_KEY",
+//   authDomain: "YOUR_PROJECT.firebaseapp.com",
+//   projectId: "YOUR_PROJECT_ID",
+//   storageBucket: "YOUR_PROJECT.appspot.com",
+//   messagingSenderId: "YOUR_SENDER_ID",
+//   appId: "YOUR_APP_ID"
+// };
