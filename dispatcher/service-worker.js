902637f702de879f2a33124545890bd978b55e37
jofforka <54028942+jofforka@users.noreply.github.com>
2026-08-16 08:57:39 +0100

    Add files via upload

---
+++ dispatcher/service-worker.js
@@ -0,0 +1,3 @@
+const CACHE="biserry-dispatcher-v1";const ASSETS=["./","./index.html","./app.js","../css/styles.css","../assets/logo.png"];
+self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener("fetch",e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
